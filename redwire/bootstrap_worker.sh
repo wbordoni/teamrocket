020cd7d95b11f3a27093af873885995af18abbdf
wbordoni <w.bordoni@protonmail.com>
2020-02-06 15:03:20 +0100

    FIX du foutoir de benjamin

--- redwire/bootstrap_worker.sh
+++ redwire/bootstrap_worker.sh
@@ -3,3 +3,7 @@
 # Set Root password
 echo "[TASK 0] Set root password"
 echo "worker" | passwd --stdin root >/dev/null 2>&1
+
+# Install prerequisites
+echo "[TASK 1] Install prerequisites"
+yum install -y -q epel-release vim wget
