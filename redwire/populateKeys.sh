5d70f27ed37f9b7f1cad095442ac6a85d67f30f8
wbordoni <w.bordoni@protonmail.com>
2020-02-06 12:31:03 +0100

    populateKeys.sh fix

--- redwire/populateKeys.sh
+++ redwire/populateKeys.sh
@@ -4,6 +4,7 @@ echo "Generate ssh-keypair"
 ssh-keygen -q -N "" -f /home/vagrant/.ssh/id_rsa
 
 echo "Copy keys to nodes"
+ssh-copy-id -i /home/vagrant/.ssh/id_rsa leader
 ssh-copy-id -i /home/vagrant/.ssh/id_rsa nexus
 ssh-copy-id -i /home/vagrant/.ssh/id_rsa gitlab
 for i in `seq 1 3`
