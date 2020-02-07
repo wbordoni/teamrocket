ec2fd40031bdd5855d710638b13d4cda27e37eb4
wbordoni <w.bordoni@protonmail.com>
2020-02-07 09:25:53 +0100

    boostrap gitlab+nexus STRIPPED !

--- redwire/bootstrap_gitlab.sh
+++ redwire/bootstrap_gitlab.sh
@@ -1,13 +1,17 @@
 # Set Root password
-echo "[TASK 12] Set root password"
+echo "[TASK 1] Set root password"
 echo "root" | passwd --stdin root >/dev/null 2>&1
 
-#Install gitlab
-echo "Install gitlab"
-yum -y install curl policycoreutils openssh-server openssh-clients postfix
-systemctl start sshd
-systemctl start postfix
-systemctl enable sshd
-systemctl enable postfix
-curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
-yum -y install gitlab-ce
+# Install prerequisites
+echo "[TASK 2] Install prerequisites"
+yum install -y -q epel-release vim wget
+
+## Install gitlab
+# echo "Install gitlab"
+# yum -y install curl policycoreutils openssh-server openssh-clients postfix
+# systemctl start sshd
+# systemctl start postfix
+# systemctl enable sshd
+# systemctl enable postfix
+# curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+# yum -y install gitlab-ce
--- redwire/bootstrap_nexus.sh
+++ redwire/bootstrap_nexus.sh
@@ -7,31 +7,31 @@ yum install -y epel-release unzip vim wget
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
 
 # Creating necessory folder structure
-mkdir -p /zone/nexus-data /opt/nexus
+# # mkdir -p /zone/nexus-data /opt/nexus
 
 # Download latest Nexus artifact
-wget -O /tmp/nexus.tar.gz http://download.sonatype.com/nexus/3/latest-unix.tar.gz
+# # wget -O /tmp/nexus.tar.gz http://download.sonatype.com/nexus/3/latest-unix.tar.gz
 
 # Extract it to /opt/nexus
-tar xvfz /tmp/nexus.tar.gz -C /opt/nexus --strip-components 1
+# # tar xvfz /tmp/nexus.tar.gz -C /opt/nexus --strip-components 1
 
 # Adding a service account for nexus
-sudo useradd --system --no-create-home nexus
+# # sudo useradd --system --no-create-home nexus
 
 # Provide necessory folder permissions
-chown -R nexus:nexus /opt/nexus
-chown -R nexus:nexus /zone/nexus-data
+# # chown -R nexus:nexus /opt/nexus
+# # chown -R nexus:nexus /zone/nexus-data
 
 # Setting up the default JDK
-alternatives --config java
+# # alternatives --config java
 
 # Setting up JAVA_HOME by adding the following line at the bottom of /etc/bashrc
-export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+# # export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
 
 # Setting up NEXUS_HOME by adding the following line at the bottom of /etc/bashrc
-export NEXUS_HOME=/opt/nexus
+# # export NEXUS_HOME=/opt/nexus
 
 # Get the updated JAVA_HOME and NEXUS_HOME into current shell
-source /etc/bashrc
+# # source /etc/bashrc
 
 
